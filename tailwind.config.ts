a2b4b38534dc14730723f776f4bee1364dd0981e
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-09-26 08:29:34 +0300

    Created project, added base styles

---
+++ tailwind.config.ts
@@ -0,0 +1,49 @@
+import type { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-marcellus)",
+      secondary: "var(--font-montserrat)",
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#473936",
+        },
+        secondary: {
+          DEFAULT: "#f2dfce",
+        },
+        accent: {
+          DEFAULT: "#f19687",
+          "100": "#f2d5c5",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [tailwindAnimate],
+};
+
+export default config;
